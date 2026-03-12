f4958a0083f5e46b9e83164fe0b3a297f4f46a0a
LaoraSad <264025963+LaoraSad@users.noreply.github.com>
2026-03-12 17:52:37 -0500

    spa

--- spa.py
+++ spa.py
@@ -1,9 +1,23 @@
 print("----BIENVENIDO A SPA LAORA----\n"
       "servicios disponibles")
 
-servicios=["1.masaje", "2.facial", "3.manicura"]
-
+servicios = ["1.masaje", "2.facial", "3.manicura"]
 
 for i in servicios:
-    print (i)
+    print(i)
+
+opcion = input("escoge el servicio que deseas: ")
+
+if opcion == "1":
+    print("servicio disponible: masaje")
+elif opcion == "2":
+    print("servicio disponible: facial")
+elif opcion == "3":
+    print("servicio disponible: manicura")
+else:
+    print("servicio no disponible")
+
+
+
+
 
