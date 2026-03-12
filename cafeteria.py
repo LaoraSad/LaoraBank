c26d249f680472faff8146dd3b3a785c905ff2b8
LaoraSad <264025963+LaoraSad@users.noreply.github.com>
2026-03-11 22:38:18 -0500

    cafetería

---
+++ cafeteria.py
@@ -0,0 +1,15 @@
+print("----BIENVENIDO A LA CAFERERÍA LAORA ----\n")
+
+print(
+"--------Menu-------\n" 
+"1.café = 4000\n"
+"2.té = 3500\n"
+"3.jugo = 5000\n"
+)
+
+opcion=input("¿Qué bebidas deseas comprar?")
+if opcion==1:
+    bebida= "café "
+    precio= 400
+    
+
