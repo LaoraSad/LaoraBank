42cc5f9a324b07668468955104a2f005529a6f9a
LaoraSad <264025963+LaoraSad@users.noreply.github.com>
2026-03-12 09:44:40 -0500

    tienda

--- tienda deportiva.py
+++ tienda deportiva.py
@@ -1,10 +1,13 @@
 print("----BIENVENIDO A TIENDA DEPORTIVA GYM----\n")
-producto=[]
+producto1=[]
 for i in range(1,7,1):
     producto = int(input(f"ingrese el precio del producto {i}: "))
     if producto > 100000:
-        producto.append()
-        print(f"estos productos son mayor a 100000 {producto}")
+        producto1.append(producto)
+if producto1:
+    print(f"productos con precio mayor a 100000: {producto1}")
+else:
+    print("no hay productos mayores a 100000")
 
        
 
